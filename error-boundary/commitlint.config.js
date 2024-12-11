590621380e30a6869609f0c8708126f07076abd2
Asel <asel.alimbaeva.kk@gmail.com>
2024-12-11 15:54:15 +0300

    test: add commitlint examOle

--- error-boundary/commitlint.config.js
+++ error-boundary/commitlint.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'refactor', 'test']],
+    'subject-case': [2, 'never', ['sentence-case']],
+  },
 };
