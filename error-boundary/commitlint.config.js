f6b4c909155818301afcf2ff793301435a5721b0
Asel <asel.alimbaeva.kk@gmail.com>
2024-12-12 11:55:37 +0300

    Keep calm and commit

--- error-boundary/commitlint.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'refactor', 'test']],
-    'subject-case': [2, 'never', ['sentence-case']],
-  },
-};
--- error-boundary/src/App.tsx
+++ error-boundary/src/App.tsx
@@ -7,7 +7,10 @@ function App() {
   const [count, setCount] = useState(0);
 
   console.log('---')
+  console.log('---')
   
+
+
   return (
     <>
       <div>
