201e09feeb2eabe849055c808b1c0074d3484c95
Asel <asel.alimbaeva.kk@gmail.com>
2024-12-16 10:36:12 +0300

    feat: add handleBreedItem

--- error-boundary/src/components/Results.tsx
+++ error-boundary/src/components/Results.tsx
@@ -1,11 +1,47 @@
 import { Component } from "react"
 
 interface ResultsProps {
-    searchValue: string;
-  }
+  searchValue: string;
+}
+
+interface CatImage {
+  id: string;
+  url: string;
+}
+
+interface ResultsState {
+  images: CatImage[];
+  isLoading: boolean;
+  error: string | null;
+}
 
 class Results extends Component<ResultsProps> {
-  state = {};
+    state: ResultsState = {
+      images: [],
+      isLoading: true,
+      error: null,
+    };
+
+  async componentDidMount() {
+    const apiKey = "live_jTLXI5GGxwquevnfnM4WJdb9R2nN2KBt7THZGwl6AYe7ChJvnQnrigW2VQp252SF";
+
+    try {
+        console.log(this.props.searchValue);
+      const response = await fetch(
+        `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${this.props.searchValue}&api_key=${apiKey}`
+      );
+
+      if (!response.ok) {
+        throw new Error("Не удалось загрузить данные.");
+      }
+
+      const data: CatImage[] = await response.json();
+      console.log({ images: data, isLoading: false });
+      this.setState({ images: data, isLoading: false });
+    } catch (error: any) {
+      this.setState({ error: error.message, isLoading: false });
+    }
+  }
 
   render(): JSX.Element {
     console.log(this.props.searchValue);
--- error-boundary/src/components/TopControls.tsx
+++ error-boundary/src/components/TopControls.tsx
@@ -1,34 +1,102 @@
-import { ChangeEvent, Component, FormEvent } from "react"
+import { ChangeEvent, Component, FormEvent, MouseEvent } from "react"
+import { CatBreed } from "../types/types";
 
 interface TopControlsProps {
   onSearch: (query: string) => void;
 }
 
+interface Breed {
+  id: string;
+  name: string;
+}
+
 class TopControls extends Component<TopControlsProps> {
   state = {
-    inputValue: "",
+    inputValue: localStorage.getItem("searchValue") ?? "",
+    idValue: localStorage.getItem("idValue") ?? "",
+    breeds: [] as Breed[],
+    inputOptions: [] as Breed[],
+    showListBreeds: false
+  }
+
+  async fetchBreeds() {
+    try {
+      const response = await fetch("https://api.thecatapi.com/v1/breeds");
+      const data: CatBreed[] = await response.json();
+
+      const dataBread: Breed[] = []
+      data.map(el => (
+        dataBread.push({id: el.id, name: el.name})
+      ))
+      this.setState({ breeds: dataBread });
+      if (!this.state.inputValue.length) {
+        this.state.inputOptions = dataBread;
+        this.state.showListBreeds = true;
+    }
+    } catch (error: any) {
+      this.setState({ error: "Ошибка загрузки списка пород" });
+    }
   }
 
   handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
+    const value = event.target.value.toLowerCase();
+    this.state.showListBreeds = true;
     this.setState({ inputValue: value });
+    const filterBreads = this.state.breeds.filter((el) => el.name.toLowerCase().includes(value))
+    this.state.inputOptions = filterBreads;
   };
 
   handleSubmit = (event: FormEvent) => {
       event.preventDefault();
       localStorage.setItem("searchValue", this.state.inputValue);
-      this.props.onSearch(this.state.inputValue); 
+      this.props.onSearch(this.state.idValue); 
+      this.state.showListBreeds = false;
+  }
+
+  handleBreedItem = (event: MouseEvent<HTMLLIElement>) => {
+    const eventId = (event.currentTarget as HTMLLIElement).dataset.id as string;
+    const eventName = (event.currentTarget as HTMLLIElement).textContent as string;
+    this.state.showListBreeds = false;
+    this.setState({
+        ...this.state,
+        showListBreeds: false,
+        idValue: eventId,
+        inputValue: eventName
+    });
+
+    localStorage.setItem("searchValue", eventName)
+    localStorage.setItem("idValue", eventId)
+  }
+
+  async componentDidMount() {
+    this.fetchBreeds();
+  }
+
+  componentWillUnmount() {
+    this.fetchBreeds();
+    this.state.showListBreeds = false;
   }
     
   render(): JSX.Element {
     return (
         <form onSubmit={this.handleSubmit}>
-            <input 
-              type="text"
-              value={this.state.inputValue}
-              onChange={this.handleChange}
-              placeholder="Введите текст для поиска"
-            />
+            <div>
+              <input 
+                type="text"
+                value={this.state.inputValue}
+                onChange={this.handleChange}
+                placeholder="Введите текст для поиска"
+              />
+              {this.state.showListBreeds && (
+                <ul>
+                    {this.state.inputOptions.map((el, index) => {
+                    return (
+                        <li key={el.id + index} data-id={el.id} onClick={this.handleBreedItem}>{el.name}</li>
+                    )
+                    })}
+                </ul>
+              )}
+            </div>
             <button type="submit">Search</button>
         </form>
     )
