7ec4c72aa985dcb61f1b0e061fb221e1a36f076e
Asel <asel.alimbaeva.kk@gmail.com>
2025-02-07 14:41:32 +0300

    refactor: changed IsLoading class componentr to function component

--- src/components/IsLoading.tsx
+++ src/components/IsLoading.tsx
@@ -1,20 +1,17 @@
-import { Component, ReactNode } from 'react';
 import './styles/isLoading.scss';
 
-class IsLoading extends Component {
-  render(): ReactNode {
-    return (
-      <div className="modal">
-        <div className="inner-modal">
-          <div className="inner">
-            <div className="load-item item-1"></div>
-            <div className="load-item item-2"></div>
-            <div className="load-item item-3"></div>
-          </div>
+const IsLoading = () => {
+  return (
+    <div className="modal">
+      <div className="inner-modal">
+        <div className="inner">
+          <div className="load-item item-1"></div>
+          <div className="load-item item-2"></div>
+          <div className="load-item item-3"></div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default IsLoading;
