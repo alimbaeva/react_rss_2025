8a972763fdf633031694015cc7f3df5693c95364
Asel <asel.alimbaeva.kk@gmail.com>
2025-02-13 20:14:33 +0300

    feat: create icones components

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import './header.scss';
+
+const Header: FC = () => {
+  return (
+    <header>
+      <div className="header">header</div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import './app.scss';
 import Page404 from './page404/Page404';
+import Header from '../components/header/Header';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Page404 />} />
