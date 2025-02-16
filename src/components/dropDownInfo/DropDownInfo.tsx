0675b3b5eff968d1c4d73be70b237e60ca085f5e
Asel <asel.alimbaeva.kk@gmail.com>
2025-02-16 13:20:57 +0300

    feat: add style for DropDownInfo

--- src/components/dropDownInfo/DropDownInfo.tsx
+++ src/components/dropDownInfo/DropDownInfo.tsx
@@ -3,19 +3,26 @@ import DownloadIcon from '../icons/DownloadIcon';
 import TrashIcon from '../icons/TrashIcon';
 import '../styles/dropDownInfo.scss';
 
+const headerIconColor = '#e67a7a';
+
 const DropDownInfo: FC = () => {
   return (
     <section>
-      <div>
-        <p>
-          <span>3</span> items selected
-        </p>
-        <button>
-          <TrashIcon fill={''} />
-        </button>
-        <button>
-          <DownloadIcon fill={''} />
-        </button>
+      <div className="down-info">
+        <div>
+          <p className="count-item">
+            <span>3</span> items selected
+          </p>
+          <button className="reveal-btn">Reveal more information</button>
+        </div>
+        <div className="button-wrapper">
+          <button className="trash-btn">
+            <TrashIcon fill={headerIconColor} />
+          </button>
+          <button className="download-btn">
+            <DownloadIcon fill={headerIconColor} />
+          </button>
+        </div>
       </div>
     </section>
   );
