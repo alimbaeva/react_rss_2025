796441d367d80e71624118deadd02f716877c080
Asel <asel.alimbaeva.kk@gmail.com>
2025-02-16 19:53:31 +0300

    refactor: move form to SearchInputs file

--- src/components/SearchInputs.tsx
+++ src/components/SearchInputs.tsx
@@ -1,9 +1,21 @@
-import { ChangeEvent, MouseEvent, FC, useState, useEffect } from 'react';
+import {
+  ChangeEvent,
+  MouseEvent,
+  FC,
+  useState,
+  useEffect,
+  FormEvent,
+} from 'react';
 import { Breed } from '../types/types';
 import './styles/topControls.scss';
 import { RootState } from '../store/store';
 import { useDispatch, useSelector } from 'react-redux';
-import { setSearchValue, setSearchValueKey } from '../store/slices/searchSlice';
+import {
+  setCleanSearch,
+  setCurrentPage,
+  setSearchValue,
+  setSearchValueKey,
+} from '../store/slices/searchSlice';
 
 const SearchInputs: FC = () => {
   const dispatch = useDispatch();
@@ -13,16 +25,22 @@ const SearchInputs: FC = () => {
   );
   const [inputOptions, setInputOptions] = useState<Breed[]>([]);
   const [showListBreeds, setShowListBreeds] = useState<boolean>(true);
+  const [valueKey, setValueKey] = useState<string>(searchValueKey);
+  const [seaValue, setSeaValue] = useState<string>(searchValue);
+  const [breedItem, setBreedItem] = useState<string>(searchValue);
 
   const handleChangeKey = (event: ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value.toLowerCase().trim();
-    dispatch(setSearchValueKey(value));
+    setValueKey(value);
+    setSeaValue('');
+    setBreedItem('');
     setShowListBreeds(false);
   };
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value.toLowerCase().trim();
-    dispatch(setSearchValue(value));
+    setSeaValue(value);
+    setValueKey('');
     setShowListBreeds(true);
 
     const filteredBreeds = breeds.filter((el) =>
@@ -38,8 +56,21 @@ const SearchInputs: FC = () => {
 
   const handleBreedItem = (event: MouseEvent<HTMLLIElement>) => {
     const eventName = event.currentTarget.textContent as string;
-    dispatch(setSearchValue(eventName));
+    setBreedItem(eventName);
+    setSeaValue(eventName);
     setShowListBreeds(false);
+  };
+
+  const handleSubmit = (event: FormEvent<Element>) => {
+    event?.preventDefault();
+
+    if (!searchValue && !searchValueKey) {
+      dispatch(setCleanSearch());
+      return;
+    }
+    if (breedItem) dispatch(setSearchValue(breedItem));
+    if (valueKey) dispatch(setSearchValueKey(valueKey));
+    dispatch(setCurrentPage(0));
   };
 
   useEffect(() => {
@@ -50,45 +81,48 @@ const SearchInputs: FC = () => {
   }, [breeds, searchValue]);
 
   return (
-    <div className="inputs-wrapper">
-      <div className="input-wrapper">
-        <p>Поиск.</p>
-        <input
-          type="text"
-          value={searchValueKey || ''}
-          onChange={handleChangeKey}
-          placeholder="Введите текст для поиска"
-        />
+    <form onSubmit={handleSubmit}>
+      <div className="inputs-wrapper">
+        <div className="input-wrapper">
+          <p>Поиск.</p>
+          <input
+            type="text"
+            value={valueKey || ''}
+            onChange={handleChangeKey}
+            placeholder="Введите текст для поиска"
+          />
+        </div>
+        <div className="input-wrapper">
+          <p>Поиск с предложениями помогает быстро найти результат.</p>
+          <input
+            type="text"
+            value={seaValue || ''}
+            onChange={handleChange}
+            placeholder="Введите текст для поиска"
+          />
+          {showListBreeds && (
+            <>
+              {inputOptions.length > 0 ? (
+                <ul className="wrapper-breed">
+                  {inputOptions.map((el) => (
+                    <li
+                      key={el.id}
+                      onClick={handleBreedItem}
+                      className="breed-item"
+                    >
+                      {el.name}
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>No breeds found</p>
+              )}
+            </>
+          )}
+        </div>
       </div>
-      <div className="input-wrapper">
-        <p>Поиск с предложениями помогает быстро найти результат.</p>
-        <input
-          type="text"
-          value={searchValue || ''}
-          onChange={handleChange}
-          placeholder="Введите текст для поиска"
-        />
-        {showListBreeds && (
-          <>
-            {inputOptions.length > 0 ? (
-              <ul className="wrapper-breed">
-                {inputOptions.map((el) => (
-                  <li
-                    key={el.id}
-                    onClick={handleBreedItem}
-                    className="breed-item"
-                  >
-                    {el.name}
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>No breeds found</p>
-            )}
-          </>
-        )}
-      </div>
-    </div>
+      <button data-testid="search-button">Search</button>
+    </form>
   );
 };
 
--- src/components/TopControl.tsx
+++ src/components/TopControl.tsx
@@ -1,42 +1,18 @@
-import { useState, FormEvent, FC } from 'react';
+import { useState, FC } from 'react';
 import { ERRORLOADING } from '../veriables';
-import { useSearch } from './context/useSearch';
 import SearchInputs from './SearchInputs';
 import './styles/topControls.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../store/store';
 import { useGetBreedsQuery } from '../store/queryApi/breedsApi';
-import { setCleanSearch } from '../store/slices/searchSlice';
 
 const TopControls: FC = () => {
-  const dispatch = useDispatch();
-  const { searchValue, searchValueKey } = useSelector(
-    (state: RootState) => state.search
-  );
   const { error } = useGetBreedsQuery(undefined);
-  const { setCurrentPage } = useSearch();
   const [errorBtn, setErrorBtn] = useState<string | null>(null);
 
   if (errorBtn) throw new Error('Имитация ошибки при клике.');
 
-  const handleSubmit = (event: FormEvent<Element>) => {
-    event?.preventDefault();
-
-    if (!searchValue && !searchValueKey) {
-      dispatch(setCleanSearch());
-      return;
-    }
-
-    setCurrentPage(0);
-    localStorage.setItem('currentPage', `${0}`);
-  };
-
   return (
     <div className="controls-wrapper">
-      <form onSubmit={handleSubmit}>
-        <SearchInputs />
-        <button data-testid="search-button">Search</button>
-      </form>
+      <SearchInputs />
       <button
         onClick={() => setErrorBtn(ERRORLOADING)}
         type="button"
