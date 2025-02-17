92caae782ef444777eaf8a21d700d5a6cbaa6a81
Asel <asel.alimbaeva.kk@gmail.com>
2025-02-17 14:26:17 +0300

    feat: add MoreInformation

--- src/components/dropDownInfo/DropDownInfo.tsx
+++ src/components/dropDownInfo/DropDownInfo.tsx
@@ -5,6 +5,7 @@ import { RootState } from '../../store/store';
 import { clearSelected } from '../../store/slices/selectedSlice';
 import '../styles/dropDownInfo.scss';
 import DownloadFile from './DownloadFile';
+import MoreInformation from './MoreInformation';
 
 const headerIconColor = '#e67a7a';
 
@@ -13,6 +14,7 @@ const DropDownInfo: FC = () => {
   const { selectedIds } = useSelector((state: RootState) => state.selected);
 
   const [count, setCount] = useState(0);
+  const [showMoreInfo, setShowMoreInfo] = useState(false);
 
   const hadleTrashSelected = () => {
     dispatch(clearSelected());
@@ -20,6 +22,7 @@ const DropDownInfo: FC = () => {
 
   useEffect(() => {
     setCount(selectedIds.length);
+    setShowMoreInfo(false);
   }, [selectedIds]);
 
   if (!count) return;
@@ -31,7 +34,17 @@ const DropDownInfo: FC = () => {
           <p className="count-item">
             <span>{count}</span> items selected
           </p>
-          <button className="reveal-btn">Reveal more information</button>
+          {showMoreInfo && (
+            <MoreInformation setShowMoreInfo={setShowMoreInfo} />
+          )}
+          {!showMoreInfo && (
+            <button
+              onClick={() => setShowMoreInfo(true)}
+              className="reveal-btn"
+            >
+              Reveal more information
+            </button>
+          )}
         </div>
         <div className="button-wrapper">
           <button onClick={hadleTrashSelected} className="trash-btn">
