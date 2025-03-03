06318bece7ebb8dec6d5276a84a6bafba1974216
Asel <asel.alimbaeva.kk@gmail.com>
2025-03-03 14:19:12 +0300

    refactor:  vitest.config.ts change

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,10 +14,10 @@ export default defineConfig({
         '**/*.spec.tsx',
         'src/__tests__/setup.ts',
       ],
-      provider: 'istanbul', // Явно указываем Istanbul как провайдер покрытия
+      provider: 'istanbul',
       thresholds: {
-        statements: 70,
-        branches: 60,
+        statements: 80,
+        branches: 70,
         functions: 70,
         lines: 70,
       },
