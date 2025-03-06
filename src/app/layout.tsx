ff1ee847a8529be41bd6ab7ea126df41f4ea42bd
Asel <asel.alimbaeva.kk@gmail.com>
2025-03-06 20:45:50 +0300

    fix: remove unnessecary path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,6 @@
 import React, { ReactNode } from 'react';
 import type { Metadata } from 'next';
-// import { Provider } from 'react-redux';
 import '@/styles/globals.scss';
-// import ErrorBoundary from '@/components/errorBoundary/ErrorBoundary';
-// import { store } from '@/store/store';
-// import { ThemeProvider } from '@/components/context/ThemeContext ';
-// import MainContent from '@/components/MainContent';
 import ClientLayout from '@/components/ClientLayout';
 
 export const metadata: Metadata = {
