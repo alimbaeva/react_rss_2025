2442cc3f426241f245f5240e4ebf75db83e18d0b
Asel <asel.alimbaeva.kk@gmail.com>
2025-03-20 09:02:17 +0300

    init: init app

---
+++ vitest.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/__tests__/setup.ts',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      include: ['**/*.tsx'],
+      exclude: [
+        '**/node_modules/**',
+        '**/*.test.tsx',
+        '**/*.spec.tsx',
+        'src/__tests__/setup.ts',
+      ],
+      provider: 'istanbul', // Явно указываем Istanbul как провайдер покрытия
+      thresholds: {
+        statements: 70,
+        branches: 60,
+        functions: 70,
+        lines: 70,
+      },
+    },
+  },
+});
